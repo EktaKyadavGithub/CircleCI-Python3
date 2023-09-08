20326cb7f969bb2ea02a3b470f2364bcaa918700
EktaKyadavGithub <144197799+EktaKyadavGithub@users.noreply.github.com>
2023-09-08 10:33:05 +0530

    added python file by ekta

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name="Ekta"
         upper_name=to_upper(name)
-        self.assertEqual(upper_name,"Ekta")
+        self.assertEqual(upper_name,"Tannu")
 
 
 if __name__=='__main__':
