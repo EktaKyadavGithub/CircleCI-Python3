27ee06ce3afb254e9cb467135445d917f89b50ea
EktaKyadavGithub <144197799+EktaKyadavGithub@users.noreply.github.com>
2023-09-09 09:47:22 +0530

    modified changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def say_hello(name):
     print(f'Hello,{name}')
 
 if __name__ =='__main__' :
-    name='TrainWithTannu'
+    name='TrainWithEkta'
     say_hello(name)
     up = to_upper(name)
     print(up)
